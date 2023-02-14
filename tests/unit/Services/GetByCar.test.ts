05fa0dcc6abea28d4be6ced7309e16ae537319b8
reinaldoper <reinaldoper83@gmail.com>
2023-02-14 15:08:27 -0400

    feat: cobrindo tests da camada service em 80%

---
+++ tests/unit/Services/GetByCar.test.ts
@@ -0,0 +1,105 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import Car from '../../../src/Domains/Car';
+import CarService from '../../../src/Services/CarServices';
+
+describe('Buscar carros cadastrados', function () {
+  const car = {
+    id: '634852326b35b59438fbea2f',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.99,
+    doorsQty: 4,
+    seatsQty: 5,
+  };
+  const carInput = {
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.99,
+    doorsQty: 4,
+    seatsQty: 5,
+  };
+  it('Retorna carro por ID', async function () {
+    const keyOutput: Car = new Car(car);
+    sinon.stub(Model, 'findOne').resolves(keyOutput);
+
+    const service = new CarService();
+    const result = await service.getByValue('634852326b35b59438fbea2f');
+
+    expect(result).to.be.deep.equal(keyOutput);
+
+    sinon.restore();
+  });
+  it('Cria um novo carro', async function () {
+    const keyOutput: Car = new Car(car);
+    sinon.stub(Model, 'create').resolves(keyOutput);
+
+    const service = new CarService();
+    const result = await service.create(carInput);
+
+    expect(result).to.be.deep.equal(car);
+
+    sinon.restore();
+  });
+  it('Retorna erro com ID inválido', async function () {
+    sinon.stub(Model, 'findOne').resolves(null);
+    try {
+      const service = new CarService();
+      await service.getByValue('634852326b35b59438fbea2fxxxx');
+    } catch (error) {
+      expect(error as Error).to.be.deep.equal({ message: 'Invalid mongo id' });
+    }
+
+    sinon.restore();
+  });
+  it('Retorna erro com ID inexistente', async function () {
+    sinon.stub(Model, 'findOne').resolves(null);
+    try {
+      const service = new CarService();
+      await service.getByValue('634852326b35b59438fbea2c');
+    } catch (error) {
+      expect(error as Error).to.be.deep.equal({ message: 'Car not found' });
+    }
+
+    sinon.restore();
+  });
+  it('Retorna lista de carros cadastrados', async function () {
+    const carInputs = [
+      {
+        id: '634852326b35b59438fbea2f',
+        model: 'Marea',
+        year: 2002,
+        color: 'Black',
+        status: true,
+        buyValue: 15.99,
+        doorsQty: 4,
+        seatsQty: 5,
+      },
+      {
+        id: '634852326b35b59438fbea31',
+        model: 'Tempra',
+        year: 1995,
+        color: 'Black',
+        buyValue: 39,
+        doorsQty: 2,
+        seatsQty: 5,
+      },
+    ];
+    const cars = carInputs.map(
+      (newcar) => new Car(newcar),
+    );
+    sinon.stub(Model, 'find').resolves(carInputs);
+
+    const service = new CarService();
+    const result = await service.getAllCars();
+
+    expect(result).to.be.deep.equal(cars);
+
+    sinon.restore();
+  });
+});
---
+++ tests/unit/Services/GetByMotorcycle.test.ts
@@ -0,0 +1,106 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import Motorcycle from '../../../src/Domains/Motorcycle';
+import MotorcycleService from '../../../src/Services/MotorcycleService';
+import IMotorcycle from '../../../src/Interfaces/IMotorcycle';
+
+describe('Busca motos cadastradas', function () {
+  const MOTO = 'Honda Cb 600f Hornet';
+  const moto: IMotorcycle = {
+    model: MOTO,
+    year: 2005,
+    color: 'Yellow',
+    status: true,
+    buyValue: 30.000,
+    category: 'Custom',
+    engineCapacity: 600,
+  };
+  const motoOutput: IMotorcycle = {
+    id: '6348513f34c397abcad040b2',
+    model: MOTO,
+    year: 2005,
+    color: 'Yellow',
+    status: true,
+    buyValue: 30.000,
+    category: 'Street',
+    engineCapacity: 600,
+  };
+  it('Retorna moto por ID', async function () {
+    const keyOutput = new Motorcycle(moto);
+    sinon.stub(Model, 'findOne').resolves(keyOutput);
+
+    const service = new MotorcycleService();
+    const result = await service.getByValue('6348513f34c397abcad040b2');
+
+    expect(result).to.be.deep.equal(keyOutput);
+
+    sinon.restore();
+  });
+  it('Retorna erro com ID inválido', async function () {
+    sinon.stub(Model, 'findOne').resolves(null);
+    try {
+      const service = new MotorcycleService();
+      await service.getByValue('6348513f34c397abcad040b2xxx');
+    } catch (error) {
+      expect(error as Error).to.be.deep.equal({ message: 'Invalid mongo id' });
+    }
+
+    sinon.restore();
+  });
+  it('Retorna erro com ID inexistente', async function () {
+    sinon.stub(Model, 'findOne').resolves(null);
+    try {
+      const service = new MotorcycleService();
+      await service.getByValue('6348513f34c397abcad040b1');
+    } catch (error) {
+      expect(error as Error).to.be.deep.equal({ message: 'Motorcycle not found' });
+    }
+
+    sinon.restore();
+  });
+  it('Cadastrar moto', async function () {
+    const keyOutput = new Motorcycle(motoOutput);
+    sinon.stub(Model, 'create').resolves(keyOutput);
+
+    const service = new MotorcycleService();
+    const result = await service.create(moto);
+
+    expect(result).to.be.deep.equal(motoOutput);
+
+    sinon.restore();
+  });
+  it('Retorna lista de motos', async function () {
+    const motors: IMotorcycle[] = [
+      {
+        id: '634852326b35b59438fbea2f',
+        model: 'Honda Cb 600f Hornet',
+        year: 2005,
+        color: 'Yellow',
+        status: true,
+        buyValue: 30.000,
+        category: 'Street',
+        engineCapacity: 600,
+      },
+      {
+        id: '634852326b35b59438fbea31',
+        model: 'Honda Cbr 1000rr',
+        year: 2011,
+        color: 'Orange',
+        status: true,
+        buyValue: 59.900,
+        category: 'Street',
+        engineCapacity: 1000,
+      },
+    ];
+    const keyOutput = motors.map((motos) => new Motorcycle(motos));
+    sinon.stub(Model, 'find').resolves(motors);
+
+    const service = new MotorcycleService();
+    const result = await service.getAllMotors();
+
+    expect(result).to.be.deep.equal(keyOutput);
+
+    sinon.restore();
+  });
+});
